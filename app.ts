974f7bcc93a49cc0765f30154136918460fbd1c1
ALTGAME <krgpsh13@gmail.com>
2023-08-30 15:52:24 +0530

    tsc

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ const num2Element=document.getElementById('num2') as HTMLInputElement
 const buttonElement = document.querySelector('button');
 
 
-function add(num1: number, num2 : number){
+function add(num1: any, num2 : any){
     return num1 +num2;
 }
 
@@ -11,5 +11,6 @@ buttonElement?.addEventListener('click', ()=>{
     const num1 =num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    
     console.log(result);
 });
